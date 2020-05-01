e576d00b2448db14cc41cc8b0262e6d9e8f5cb54
Hamada <defjamvk@gmail.com>
2020-05-01 18:37:02 +0200

    New verison
    New version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,32 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
-import { HttpClientModule } from "@angular/common/http";
-import { AppComponent } from "./app.component";
-import { ListaComponent } from "./lista/lista.component";
-import { FormularioComponent } from "./formulario/formulario.component";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+
+// components
+import { AppComponent } from './app.component';
+import { AppHeaderComponent } from './app-header.component';
+
+// modules
+import { AuthModule } from './auth';
+import { FirebaseModule } from './firebase';
+import { TasksModule } from './tasks';
+
 
 @NgModule({
-  declarations: [AppComponent, ListaComponent, FormularioComponent],
-  imports: [BrowserModule, FormsModule, HttpClientModule],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ],
+  declarations: [
+    AppComponent,
+    AppHeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([], {useHash: false}),
+
+    AuthModule,
+    FirebaseModule,
+    TasksModule
+  ]
 })
-export class AppModule {}
+export class AppModule { }
