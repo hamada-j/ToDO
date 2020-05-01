0b0815b92339677836c27d3e73e57685ff8d0f06
Hamada <defjamvk@gmail.com>
2020-05-01 19:12:35 +0200

    update readme
    update readme

--- src/environments/firebase.ts
+++
@@ -1,6 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyDeXfupanzFw4v40n2q_q4N__YhuER4GSM",
-  authDomain: "todo-in-fire.firebaseapp.com",
-  databaseURL: "https://todo-in-fire.firebaseio.com",
-  storageBucket: "todo-in-fire.appspot.com",
-};
